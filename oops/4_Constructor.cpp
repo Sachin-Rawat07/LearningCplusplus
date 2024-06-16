3a3dd9f8e1374f95e3064a45bccfb66224f95fa0
Sachin-Rawat07 <sachinrawatsr07@gmail.com>
2024-06-16 08:11:30 +0530

    constructor

--- oops/4_Constructor.cpp
+++ oops/4_Constructor.cpp
@@ -9,7 +9,7 @@ class Student
     int age, roll_no;
 
     public:
-    // Defualt Constructor
+    // Default Constructor
     Student(){
 
     cout << "constructor is called" << endl;
@@ -18,15 +18,35 @@ class Student
     roll_no = 42;
     }
 
+    //Parameterized Constructor
+    Student(string a,int b,int c){
+        name=a;
+        age=b;
+        roll_no=c;
+    }
 
-    
+
+    //Constructor Overloading
+    Student(string name){//for others parameter it will store garbage value
+       this->name=name;
+    }
+
     void display() {
         cout << "Name is " << name << " age is " << age << " roll no is " << roll_no << endl;
     }
     
+
+    //inline Constructor
+    inline Student(string a, int b):name(a),age(b){}
 };
 
 int main(){    
     Student A1;
+    Student A2("Rohit",40,45);
     A1.display();
+    A2.display();
+    Student A3("Ravi");
+    A3.display();
+    Student A4("Rohan",13);
+    A4.display();
 }
